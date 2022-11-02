c57e0a91cbacc3919034cb3fd3d2ee14863a9538
mulnazb <mulnazb@gmail.com>
2022-11-02 18:12:03 -0400

    commiting whole file hello

---
+++ hello/src/main/java/helloJI/Hello.java
@@ -0,0 +1,21 @@
+package helloJI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+		
+		
+	}
+
+}
