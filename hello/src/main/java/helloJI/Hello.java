51dd436f6255f901ff927a222fbe28af4ee175f5
mulnazb <mulnazb@gmail.com>
2022-11-08 14:59:09 -0500

    1st change

--- hello/src/main/java/helloJI/Hello.java
+++ hello/src/main/java/helloJI/Hello.java
@@ -1,20 +1,25 @@
 package helloJI;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hello {
 	
-	public static void main(String[] args) {
-
-
+	
+	  @Test
+	    public static void getDriver() throws Exception {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://google.com");
-		
+		driver.findElement(By.name("q")).sendKeys("New York", Keys.ENTER);
+		Thread.sleep(2000);
+		driver.close();
 		
 	}
 
